e3a5c39ad9005afe89ac904b49d7b7662c7f14c2
Pranav Asthana <asthana_pranav@yahoo.co.in>
2019-02-12 16:57:20 +0530

    Improved speed

--- Assignment1/apriori.py
+++ Assignment1/apriori.py
@@ -1,9 +1,11 @@
 from tqdm import tqdm
 from pprint import pprint
+import sys
 
 # data = list(filter( lambda x: not x == '', open('test_dataset.csv', 'r').read().split('\n')))
-data = list(filter( lambda x: not x == '', open('groceries.csv', 'r').read().split('\n')))
-minsup = 500
+# minsup = 2
+data = list(filter(lambda x: not x == '', open('groceries.csv', 'r').read().split('\n')))
+minsup = 100
 
 def support_count(itemset):
     sc = 0
@@ -12,50 +14,59 @@ def support_count(itemset):
             sc += 1
     return sc
 
-def generate_itemsets(C1, k, Cj=set()):
+def generate_itemsets(L1, k, Lj=set()):
     if k == 2:
-        Cj = C1
-    Ck = []
-    for itemset in tqdm(Cj):
-        for item in C1:
+        Lj = L1
+    Ck = {}
+    for itemset in tqdm(Lj):
+        for item in L1:
             new_itemset = frozenset(itemset.union(frozenset(item)))
-            sc = support_count(new_itemset)
-            if sc >= minsup and len(new_itemset)==k:
-                Ck.append((new_itemset, sc))
-    return frozenset(Ck)
+            Ck[new_itemset] = 0
+
+    for t in tqdm(data):
+        t = set(t.split(','))
+        for c in Ck.keys():
+            if t.intersection(c) == c: # if c in t
+                Ck[c] += 1
+    Lk = [(c[0],c[1]) for c in tqdm(Ck.items()) if c[1]>=minsup and len(c[0])==k]
+    return frozenset(Lk)
 
 def main():
-    L1 = []
-    _ = [L1.extend(d.split(',')) for d in data]
-    L1 = set([frozenset([c]) for c in L1])
+    global minsup
+    if len(sys.argv) > 1:
+        minsup = int(sys.argv[1])
 
     C1 = []
-    for c in tqdm(L1):
+    _ = [C1.extend(d.split(',')) for d in data]
+    C1 = set([frozenset([c]) for c in C1])
+
+    L1 = []
+    for c in tqdm(C1):
         sc = support_count(c)
         if sc >= minsup:
-            C1.append((c, sc))
-    lC1 = len(C1)
-    print("Number of length 1 itemsets = {}".format(lC1))
+            L1.append((c, sc))
+    lL1 = len(L1)
+    print("Number of length 1 itemsets = {}".format(lL1))
 
-    total = lC1
-    f = open("itemsets", 'w')
-    f.write("Length 1: " + str(lC1) + "\n")
-    _ = [f.write(",".join(c[0])+" ({})\n".format(c[1])) for c in C1]
+    total = lL1
+    f = open("output/Freq_Items_sup:"+str(minsup), 'w')
+    f.write("Length 1: " + str(lL1) + "\n")
+    _ = [f.write(",".join(c[0])+" ({})\n".format(c[1])) for c in L1]
     f.write('\n')
-    Cprev = C1
+    Lprev = L1
     for i in range(2, len(C1)+1):
-        Ci = generate_itemsets(list(zip(*C1))[0], i, list(zip(*Cprev))[0])
-        l = len(Ci)
+        Li = generate_itemsets(list(zip(*L1))[0], i, list(zip(*Lprev))[0])
+        l = len(Li)
         total += l
         print("Number of length {} itemsets = {}".format(i, l))
         if l == 0:
             break
         f.write("Length "+ str(i) +": " + str(l) + '\n')
-        _ = [f.write(",".join(c[0])+" ({})\n".format(c[1])) for c in Ci]
-        # f.write(str(Ci))
+        _ = [f.write(",".join(c[0])+" ({})\n".format(c[1])) for c in Li]
+        # f.write(str(Li))
         f.write('\n')
-        Cprev = Ci
-    print("Total number of itemsets generated: ", total)
+        Lprev = Li
+    print("Total number of frequent itemsets =", total)
 
 
 if __name__ == '__main__':

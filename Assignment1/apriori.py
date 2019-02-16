00a5c9c08bc2c05c04c3ee054c65c16e50ad28f3
Uttara Ravi <uttararavi9@gmail.com>
2019-02-16 16:38:32 +0530

    assn rule generation done

--- Assignment1/apriori.py
+++ Assignment1/apriori.py
@@ -2,10 +2,11 @@ from tqdm import tqdm
 from pprint import pprint
 import sys
 
-# data = list(filter( lambda x: not x == '', open('test_dataset.csv', 'r').read().split('\n')))
-# minsup = 2
-data = list(filter(lambda x: not x == '', open('groceries.csv', 'r').read().split('\n')))
-minsup = 100
+data = list(filter( lambda x: not x == '', open('test_dataset.csv', 'r').read().split('\n')))
+minsup = 2
+# data = list(filter(lambda x: not x == '', open('groceries.csv', 'r').read().split('\n')))
+# minsup = 100
+minconf = 50
 
 def support_count(itemset):
     sc = 0
@@ -30,13 +31,70 @@ def generate_itemsets(Lj, k):
         for c in Ck.keys():
             if t.intersection(c) == c: # if c in t
                 Ck[c] += 1
-    Lk = [(c[0],c[1]) for c in tqdm(Ck.items()) if c[1]>=minsup and len(c[0])==k]
+    Lk = [(c[0],c[1]) for c in tqdm(Ck.items()) if c[1]>=minsup]
     return frozenset(Lk)
+
+def find_confidence(rule, freq_items_sup):
+
+    confidence = freq_items_sup[rule[0].union(rule[1])]/freq_items_sup[rule[0]]
+    confidence *= 100
+    # print("confidence")
+    # print(confidence)
+    return confidence
+
+def generate_rules(itemset, freq_items_sup, final_rules):
+    LPrev = []
+    X = {}  #dictionary that stores rule(tuple) with it's confidence
+    for i in itemset:
+        t = frozenset([i])
+        u = (itemset.difference(t) ,t)
+        if find_confidence(u,freq_items_sup) >= minconf:
+            X[u] = find_confidence(u,freq_items_sup)
+            LPrev.append(u)
+
+    final_rules.extend(LPrev)
+
+    Li = []
+    #to make sure no repeated elements
+    LPrev = list(set(LPrev))
+
+    while 1:
+        if len(LPrev) <= 1:
+            # print("Reached end @@")    
+            return X
+        for i in LPrev:
+            for j in LPrev:
+                if i == j:
+                    continue
+                #combine the two rules
+                rhs = i[1].union(j[1])
+                lhs = i[0].union(j[0])
+                lhs = lhs.difference(rhs)
+
+                if len(lhs) == 0:
+                    # print("Reached end")
+                    return X
+
+                s = (lhs, rhs)
+                fc = find_confidence(s, freq_items_sup)
+                if  fc >= minconf:
+                    X[s] = fc
+                    Li.append(s)
+
+        #to make sure no repeated elements
+        Li = list(set(Li))
+        LPrev = Li
+        final_rules.extend(LPrev)
 
 def main():
     global minsup
-    if len(sys.argv) > 1:
+    global minconf
+    if len(sys.argv) > 2:
         minsup = int(sys.argv[1])
+        minconf = int(sys.argv[2])
+
+    elif len(sys.argv) > 1:
+        minsup = int(sys.argv[1]) 
 
     C1 = []
     _ = [C1.extend(d.split(',')) for d in data]
@@ -55,9 +113,10 @@ def main():
     f.write("Length 1: " + str(lL1) + "\n")
     _ = [f.write(",".join(c[0])+" ({})\n".format(c[1])) for c in L1]
     f.write('\n')
-    Lprev = L1
+    L = [frozenset(L1)]
+
     for i in range(2, len(C1)+1):
-        Li = generate_itemsets(list(zip(*Lprev))[0], i)
+        Li = generate_itemsets(list(zip(*L[-1]))[0], i)
         l = len(Li)
         total += l
         print("Number of length {} itemsets = {}".format(i, l))
@@ -67,9 +126,31 @@ def main():
         _ = [f.write(",".join(c[0])+" ({})\n".format(c[1])) for c in Li]
         # f.write(str(Li))
         f.write('\n')
-        Lprev = Li
-    print("Total number of frequent itemsets =", total)
+        L.append(Li)
+    print("Total number of frequent itemsets = ", total)
 
+    # print(L)
+    #
+    freq_items_sup = {}
 
+    for i in range(0, len(L)):
+        for c in L[i]:
+            freq_items_sup[c[0]] = c[1]
+
+    final_rules = []
+
+    X = {}
+    for i in range(1, len(L)):
+        for c in L[i]:
+            X.update(generate_rules(c[0], freq_items_sup, final_rules))
+
+    # print("!!!!!!!!!!! final_rules !!!!!!!!!!!!!!!!!!")
+
+    f = open("output/Assn_Rules_sup:{},conf:{}".format(minsup, minconf), 'w')
+    for elem in final_rules:
+        f.write("{} ({}) --> {} ({}) - conf({:.2f})\n".format(set(elem[0]), freq_items_sup[elem[0]], set(elem[1]), freq_items_sup[elem[1]], X[elem]))
+
+    f.close()
 if __name__ == '__main__':
     main()
+
